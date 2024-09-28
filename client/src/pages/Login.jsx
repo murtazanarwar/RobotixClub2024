e4615807f4901345611181fba598e328e23aea30
Ayush05m <ayush05m@gmail.com>
2024-09-28 21:14:21 +0530

    updates

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const [success, setSuccess] = useState('');
-  
+
   const [userId, setUserid] = useState();
   const [username, setUsername] = useState();
 
@@ -45,64 +45,66 @@ const Login = () => {
 
     }
   };
-  dispatch(signInSuccess([userId, username ]))
+  dispatch(signInSuccess([userId, username]))
 
   return (
     <>
-      <div className="login-form max-w-md mx-auto bg-gray-900 p-8 mt-10 rounded-lg shadow-lg">
-        <h2 className="text-3xl font-semibold text-center text-yellow-500 mb-6">Login</h2>
-        <form onSubmit={handleLogin} className="space-y-6">
-          <div>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Email"
-              required
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-yellow-500"
-            />
-          </div>
-          <div>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              required
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-yellow-500"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-yellow-500 text-white font-semibold py-2 rounded-md hover:bg-yellow-600 transition duration-300 transform-gpu animate-slide-up"
-            style={{ animationDelay: '0.2s' }}
-          >
-            Login
-          </button>
+      <div className="loginWrap flex">
+        <div className="login-form max-w-md mx-auto bg-gray-900 p-8 rounded-lg shadow-lg">
+          <h2 className="text-3xl font-semibold text-center text-yellow-500 mb-6">Login</h2>
+          <form onSubmit={handleLogin} className="space-y-6">
+            <div>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+                required
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-yellow-500"
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-yellow-500"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-yellow-500 text-white font-semibold py-2 rounded-md hover:bg-yellow-600 transition duration-300 transform-gpu animate-slide-up"
+              style={{ animationDelay: '0.2s' }}
+            >
+              Login
+            </button>
 
-          {/* Forgot Password */}
-          {/* <div className="form-footer text-center animate-fade-in" style={{ animationDelay: '0.3s' }}>
+            {/* Forgot Password */}
+            {/* <div className="form-footer text-center animate-fade-in" style={{ animationDelay: '0.3s' }}>
             <a href="/forgot-password" className="text-yellow-500 hover:underline">Forgot Password?</a>
           </div> */}
-          <div
-            className="social-login text-center animate-fade-in mt-6"
-            style={{ animationDelay: '0.4s' }}
-          >
-            <p className="text-gray-600">Or login with:</p>
-            <div className="social-buttons mt-4">
-              <button
-                className="bg-red-500 text-white font-semibold py-2 px-4 rounded-md hover:bg-red-600 transition duration-300"
-              >
-                Google
-              </button>
+            <div
+              className="social-login text-center animate-fade-in mt-6"
+              style={{ animationDelay: '0.4s' }}
+            >
+              <p className="text-gray-600">Or login with:</p>
+              <div className="social-buttons mt-4">
+                <button
+                  className="bg-red-500 text-white font-semibold py-2 px-4 rounded-md hover:bg-red-600 transition duration-300"
+                >
+                  Google
+                </button>
+              </div>
             </div>
-          </div>
-          <div className="text-center mt-6">
-            <p className="text-yellow-500">
-              New User? <Link to="/sign-up" className="text-yellow-500 hover:underline">Sign Up</Link>
-            </p>
-          </div>
-        </form>
+            <div className="text-center mt-6">
+              <p className="text-yellow-500">
+                New User? <Link to="/sign-up" className="text-yellow-500 hover:underline">Sign Up</Link>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
